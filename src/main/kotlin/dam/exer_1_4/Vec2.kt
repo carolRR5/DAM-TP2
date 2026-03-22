07b37be2ee5e4556d3aaab927c8d6c93e9a90f5a
Carolina Raposo <carocaraposo2005@gmail.com>
2026-03-22 22:38:17 +0000

    Added magnitude, dot, normalized and compareTo functions to Vec2

--- src/main/kotlin/dam/exer_1_4/Vec2.kt
+++ src/main/kotlin/dam/exer_1_4/Vec2.kt
@@ -8,5 +8,15 @@ data class Vec2(val x: Double, val y: Double): Comparable<Vec2> {
     operator fun times(scalar: Double): Vec2 = Vec2(x * scalar, y * scalar)
     operator fun unaryMinus(): Vec2 = Vec2(-x, -y)
 
+    fun magnitude(): Double = sqrt(x * x + y * y)
+    fun dot(other: Vec2): Double = x * other.x + y * other.y
+    fun normalized(): Vec2 {
+        val mag = magnitude()
+        if (mag == 0.0) throw IllegalArgumentException("Cannot normalize a zero vector")
+        return Vec2(x / mag, y / mag)
+    }
 
+    override fun compareTo(other: Vec2): Int {
+        return magnitude().compareTo(other.magnitude())
+    }
 }
