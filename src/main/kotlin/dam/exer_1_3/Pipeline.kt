6046a6a86b968aa694e2873918c1b871d1362b69
Carolina Raposo <carocaraposo2005@gmail.com>
2026-03-22 21:16:00 +0000

    added execute and describe functions to Pipeline class

--- src/main/kotlin/dam/exer_1_3/Pipeline.kt
+++ src/main/kotlin/dam/exer_1_3/Pipeline.kt
@@ -6,4 +6,18 @@ class Pipeline {
     fun addStage(name: String, transform: (List<String>) -> List<String>) {
         stagesList.add(name to transform)
     }
+
+    fun execute(input: List<String>): List<String> {
+        var currentList = input
+        stagesList.forEach { step ->
+            currentList = step.second(currentList)
+        }
+        return currentList
+    }
+
+    fun describe() {
+        stagesList.forEachIndexed { index, stage ->
+            println("${index + 1}. ${stage.first}")
+        }
+    }
 }
