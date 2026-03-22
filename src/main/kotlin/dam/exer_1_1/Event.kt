7b6b519bee03cb112b7dfb310fd223607ba683d3
Carolina Raposo <carocaraposo2005@gmail.com>
2026-03-22 17:31:54 +0000

    added filterByUser extension function

--- src/main/kotlin/dam/exer_1_1/Event.kt
+++ src/main/kotlin/dam/exer_1_1/Event.kt
@@ -6,3 +6,12 @@ sealed class Event {
     class Logout (val username: String, val timestamp: Long) : Event()
 }
 
+fun List<Event>.filterByUser (username: String) : List<Event> =
+    this.filter { event ->
+        when (event){
+            is Event.Login -> event.username == username
+            is Event.Purchase -> event.username == username
+            is Event.Logout -> event.username == username
+        }
+    }
+
