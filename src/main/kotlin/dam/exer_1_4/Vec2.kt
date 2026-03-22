719b360f400bf880e216146834daf231b77ea97d
Carolina Raposo <carocaraposo2005@gmail.com>
2026-03-22 22:55:43 +0000

    complete exercise 1.4. Added get operator and main to test Vec2 class

--- src/main/kotlin/dam/exer_1_4/Vec2.kt
+++ src/main/kotlin/dam/exer_1_4/Vec2.kt
@@ -19,4 +19,33 @@ data class Vec2(val x: Double, val y: Double): Comparable<Vec2> {
     override fun compareTo(other: Vec2): Int {
         return magnitude().compareTo(other.magnitude())
     }
+
+    operator fun get(index: Int): Double = when (index) {
+        0 -> x
+        1 -> y
+        else -> throw IndexOutOfBoundsException("Index $index is out bounds for Vec2")
+    }
+}
+
+fun main() {
+    val a = Vec2(3.0, 4.0)
+    val b = Vec2(1.0, 2.0)
+
+    println("a = $a")
+    println("b = $b")
+    println("a + b = ${a + b}")
+    println("a - b = ${a - b}")
+    println("a * 2.0 = ${a * 2.0}")
+    println("-a = ${-a}")
+    println("|a| = ${a.magnitude()}")
+    println("a dot b = ${a.dot(b)}")
+    println("norm(a) = ${a.normalized()}")
+    println("a[0] = ${a[0]}")
+    println("a[1] = ${a[1]}")
+    println("a > b = ${a > b}")
+    println("a < b = ${a < b}")
+
+    val vectors = listOf(Vec2(1.0, 0.0), Vec2(3.0, 4.0), Vec2(0.0, 2.0))
+    println("Longest = ${vectors.max()}")
+    println(" Shortest = ${vectors.min()}")
 }
