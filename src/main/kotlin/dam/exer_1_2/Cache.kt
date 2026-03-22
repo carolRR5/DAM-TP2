bb67496e6f1d86570833736022b76bc0d4a552c5
Carolina Raposo <carocaraposo2005@gmail.com>
2026-03-22 19:20:52 +0000

    added get and evict functions to Cache class

--- src/main/kotlin/dam/exer_1_2/Cache.kt
+++ src/main/kotlin/dam/exer_1_2/Cache.kt
@@ -6,4 +6,19 @@ class Cache<K: Any, V: Any> () {
     fun put (key: K, value: V){
         map[key] = value
     }
+
+    fun get(key: K): V?{
+        if (map[key] != null) {
+            return map[key]
+        }
+        return null
+    }
+
+    fun evict(key: K) {
+        if (map[key] != null) {
+            map.remove(key)
+        } else {
+            println("Entry doesn't exist in cache.")
+        }
+    }
 }
