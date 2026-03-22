cc8f19a881322916923d3aa2f940590c5dd56346
Carolina Raposo <carocaraposo2005@gmail.com>
2026-03-22 17:27:38 +0000

    initial commit: added Event sealed class with Login, Purchase and Logout subclasses

---
+++ src/main/kotlin/dam/exer_1_1/Event.kt
@@ -0,0 +1,8 @@
+package dam.exer_1_1
+
+sealed class Event {
+    class Login (val username: String, val timestamp: Long) : Event()
+    class Purchase (val username: String, val amount: Double, val timestamp: Long) : Event()
+    class Logout (val username: String, val timestamp: Long) : Event()
+}
+
